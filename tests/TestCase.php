8814a4ae20e9345031d33155f48e6b7b0d5a5331
Milwad <milwad.dev@gmail.com>
2024-09-18 11:52:05 +0330

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,43 @@
 
 namespace Tests;
 
-use Orchestra\Testbench\TestCase as Orchestra;
+use Binafy\ArtisanFinder\Providers\ArtisanFinderServiceProvider;
+use Illuminate\Encryption\Encrypter;
 
-abstract class TestCase extends Orchestra
+abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    //
+    /**
+     * Get package providers.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array<int, string>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            ArtisanFinderServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Set default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        // Set app key
+        $app['config']->set('app.key', 'base64:'.base64_encode(
+            Encrypter::generateKey(config()['app.cipher'])
+        ));
+    }
 }
