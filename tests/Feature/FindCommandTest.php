d30fe7153c9da1cf939ac794017b12d2bc1498e4
Milwad <milwad.dev@gmail.com>
2024-09-23 18:14:33 +0330

    Update FindCommandTest.php

--- tests/Feature/FindCommandTest.php
+++ tests/Feature/FindCommandTest.php
@@ -1,9 +1,13 @@
 <?php
 
 use Binafy\ArtisanFinder\Commands\FindCommand;
+use Laravel\Prompts\Key;
+use Laravel\Prompts\Prompt;
 use function Pest\Laravel\artisan;
 
 test('find command work correctly', function () {
+    Prompt::fake(['u', 'e', Key::DOWN, Key::ENTER]);
+
     artisan(FindCommand::class)
         ->expectsQuestion('Search for a command', 'event:clear')
         ->assertExitCode(0);
